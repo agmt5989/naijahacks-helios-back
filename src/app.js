9e35677450064ab107c1a83df2f9372a0e0dfa78
Mike Ajala <agmt5989@gmail.com>
2019-12-13 17:04:39 +0100

    nada

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const userRouter = require('./routers/user');
+const {router} = require('./routers/user');
 const port = process.env.PORT;
 require('./db/db')
 
 app = express();
 
 app.use(express.json());
-app.use(userRouter);
+app.use(router);
 
 app.listen(port, () => {
 	console.log(`Your API is listening on ${port}`);
